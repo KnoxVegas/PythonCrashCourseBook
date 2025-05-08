faf504b62d1c3bca1ed746232cc8d2fdd07bdbe0
Cody Wilson <codywayne@gmail.com>
2025-05-08 15:53:45 -0400

    Working with lists.

---
+++ chapter_03/tryitout/tryit3_10.py
@@ -0,0 +1,4 @@
+guests = ['Mike', 'Jana', 'Caitlin', 'Laramie', 'Jessica']
+
+total_guests = len(guests)
+print(f"I'm inviting a total of {total_guests} guests to my party!")
---
+++ chapter_03/tryitout/tryit3_8.py
@@ -0,0 +1,12 @@
+locations = ['germany', 'switzerland', 'netherlands', 'ireland', 'austria', 'japan']
+print(locations)
+print(sorted(locations))
+print(locations)
+locations.reverse()
+print(locations)
+locations.reverse()
+print(locations)
+locations.sort()
+print(locations)
+locations.sort(reverse=True)
+print(locations)
